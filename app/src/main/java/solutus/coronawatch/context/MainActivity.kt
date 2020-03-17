4e67e6c52d0879a54747378b2306c8457f7f1ae8
Computer <ga_mazrou@esi.dz>
2020-03-17 19:28:53 +0000

    Structure of code

--- app/src/main/java/solutus/coronawatch/context/MainActivity.kt
+++ app/src/main/java/solutus/coronawatch/context/MainActivity.kt
@@ -1,12 +1,19 @@
-package solutus.coronawatch
+package solutus.coronawatch.context
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import solutus.coronawatch.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*button.setOnClickListener{
+            val intent = Intent(this, SecondMainActivity::class.java)
+            startActivity(intent)
+        }*/
     }
+
 }
---
+++ app/src/main/java/solutus/coronawatch/context/SecondMainActivity.kt
@@ -0,0 +1,13 @@
+package solutus.coronawatch.context
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import solutus.coronawatch.R
+
+class SecondMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_main)
+    }
+}
