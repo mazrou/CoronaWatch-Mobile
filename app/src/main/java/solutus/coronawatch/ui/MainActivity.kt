d4b7ab718547277b0e589396d82fdc5247917557
ourdjini aymen <ga_ourdjini@esi.dz>
2020-03-22 14:23:19 +0100

    bottom navigation

--- app/src/main/java/solutus/coronawatch/ui/MainActivity.kt
+++ app/src/main/java/solutus/coronawatch/ui/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.coronawtch_mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.text.SpannableString
+import android.text.style.AlignmentSpan
 import android.view.Menu
-import androidx.fragment.app.Fragment
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -11,65 +17,85 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.coronawatch_mobile.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import solutus.coronawatch.ui.health.HealthFragment
-import solutus.coronawatch.ui.home.HomeFragment
-import solutus.coronawatch.ui.info.InfoFragment
-import solutus.coronawatch.ui.map.MapFragment
-import solutus.coronawatch.ui.user.UserFragment
+
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var navController:NavController
-
-
-  private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-      item -> when(item.itemId){
-            R.id.healthFragment ->{
-                replaceFragment(HealthFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.mapFragment ->{
-                replaceFragment(MapFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-             R.id.userFragment ->{
-                replaceFragment(UserFragment())
-                return@OnNavigationItemSelectedListener true
-             }
-            R.id.infoFragment ->{
-                replaceFragment(InfoFragment())
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.homeFragment ->{
-                replaceFragment(HomeFragment())
-                return@OnNavigationItemSelectedListener true
-             }
-      }
-       return@OnNavigationItemSelectedListener false
-  }
-
+    private lateinit var toolbar: Toolbar
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+            item -> when(item.itemId){
+        R.id.healthFragment ->{
+           replaceFragment(HealthFragment)
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.mapFragment ->{
+            replaceFragment(MapFragment())
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.userFragment ->{
+            replaceFragment(UserFragment())
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.infoFragment ->{
+            replaceFragment(InfoFragment())
+            return@OnNavigationItemSelectedListener true
+        }
+        R.id.homeFragment ->{
+            replaceFragment(HomeFragment())
+            return@OnNavigationItemSelectedListener true
+        }
+    }
+        return@OnNavigationItemSelectedListener false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar=findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        supportActionBar?.setTitle(R.string.app_name)
+        supportActionBar?.setLogo(R.mipmap.ic_logo_corona_blanc)
 
-        /*navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         bottom_nav.setupWithNavController(navController)
         NavigationUI.setupActionBarWithNavController(this,navController) */
         replaceFragment(HomeFragment())
         bottom_nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
     }
-
- /*  override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(null,navController)
-    }*/
 
     private fun replaceFragment (fragment: Fragment){
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.nav_host_fragment , fragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater :MenuInflater = menuInflater
+        inflater.inflate(R.menu.top_nav,menu)
+        val switchOnOffItem = menu?.findItem(R.id.active_notifications)
+        switchOnOffItem?.setActionView(R.layout.switch_layout)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.notification_item -> {
+                Toast.makeText(this, "notification", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.help -> {
+                Toast.makeText(this, "help", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.logout -> {
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 
