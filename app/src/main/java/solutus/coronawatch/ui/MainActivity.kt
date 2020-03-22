ace0b3d124b7f8c8760d382445ba60dcfb0a2107
ourdjini aymen <ga_ourdjini@esi.dz>
2020-03-22 13:06:30 +0100

    tool bar

--- app/src/main/java/solutus/coronawatch/ui/MainActivity.kt
+++ app/src/main/java/solutus/coronawatch/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.coronawtch_mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.text.SpannableString
+import android.text.style.AlignmentSpan
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -9,6 +16,7 @@ import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.coronawatch_mobile.R
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController:NavController
@@ -18,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         toolbar=findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        supportActionBar?.setTitle(R.string.app_name)
+        supportActionBar?.setLogo(R.mipmap.ic_logo_corona_blanc)
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         bottom_nav.setupWithNavController(navController)
@@ -27,4 +37,36 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(null,navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater :MenuInflater = menuInflater
+        inflater.inflate(R.menu.top_nav,menu)
+        val switchOnOffItem = menu?.findItem(R.id.active_notifications)
+        switchOnOffItem?.setActionView(R.layout.switch_layout)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.notification_item -> {
+                Toast.makeText(this, "notification", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.help -> {
+                Toast.makeText(this, "help", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.logout -> {
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
